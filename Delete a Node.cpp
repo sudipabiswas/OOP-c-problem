626a940f6323223675b7e00b859fdd5521904203
sudipabiswas <93758248+sudipabiswas@users.noreply.github.com>
2024-09-25 14:55:20 +0600

    Add files via upload

---
+++ Delete a Node.cpp
@@ -0,0 +1,62 @@
+
+#include<iostream>
+using namespace std;
+class node
+{public:
+
+    int data;
+    node * next;
+};
+int main()
+{
+    int n;
+    cout<<"Enter node size:";
+    cin>>n;
+    node *head=NULL;
+    node *temp=NULL;
+    for (int i=0; i<n; i++)
+    {
+        if(head== NULL)
+        {
+            head = new node();
+            cin>>head->data;
+            head->next = NULL;
+            temp=head;
+        }
+        else
+        {
+            node * newitem=new node();
+            cin>>newitem-> data;
+            newitem->next= NULL;
+            temp->next=newitem;
+            temp=newitem;
+        }
+    }
+    cout<<"\nInitial linked list:"<<endl;
+    node *curr=head;
+    while(curr!= NULL)
+    {
+        cout<<curr->data<<endl;
+       curr = curr->next;
+    }
+    cout<<"______________";
+
+    node *curr2;
+    node *temp2= head;
+    int p;
+    cin>>p;
+    for(int i=0;i<p-1;i++)
+    {
+       temp2=temp2->next;
+    }
+    curr2= temp2->next;
+    temp2->next=curr2->next;
+    delete(curr2);
+    cout<<"\nAfter deleting node at position "<<p<<":"<<endl;
+      node *curr3=head;
+    while(curr3!= NULL)
+    {
+        cout<<curr3->data<<endl;
+       curr3 = curr3->next;
+    }
+}
